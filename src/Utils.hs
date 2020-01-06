cbdc9c1f5dce3e02ab6cdeb77deca6b5720d790c
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-01-07 12:40:25 +1300

    randh

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,10 @@
 module Utils where
 
 -- Imports
+import Control.Monad.Primitive
 import qualified Data.Vector.Unboxed as U
+import System.Random.MWC
+import System.Random.MWC.Distributions
 
 
 -- logsumexp of a vector
@@ -12,4 +15,18 @@ logsumexp xs =
         ys      = U.map (\x -> exp $ x - biggest) xs
     in
         biggest + log(U.sum ys)
+
+
+
+-- New year, new randh
+randh :: PrimMonad m
+      => Gen (PrimState m)
+      -> m Double
+randh rng = do
+
+    -- Reflected exponential of scale length 3
+    -- that maxes out at 1.
+    e <- (\u -> 1.0 + 3.0*log(u)) <$> uniform rng
+    let scale = 10.0**e
+    (* scale) <$> standard rng
 
